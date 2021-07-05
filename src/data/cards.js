265b8178099f07a43eafbe6873fd9456d1bebf2e
Zen Tang <zen@wayve.ai>
2021-07-05 15:39:12 -0500

    tabs -> cards
    more

---
+++ src/data/cards.js
@@ -0,0 +1,43 @@
+export const cards = [
+  {
+    img: 'img/cover/cover3.jpeg',
+    time: 'an hour left',
+    title: 'Flowers in Concrete',
+    autor: '@l1rose',
+    price: '3.19 ETH',
+    likes: 27,
+  },
+  {
+    img: 'img/cover/cover4.jpg',
+    time: 'an hour left',
+    title: 'Industrial Revolution',
+    autor: '@l1rose',
+    price: '1.11 ETH',
+    likes: 27,
+  },
+  {
+    img: 'img/cover/cover1.jpeg',
+    time: 'an hour left',
+    title: 'Walking on air',
+    autor: '@l1rose',
+    price: '3.19 ETH',
+    likes: 187,
+    hasClock: true,
+  },
+  {
+    img: 'img/cover/cover5.jpg',
+    time: 'an hour left',
+    title: 'I Love You',
+    autor: '@l1rose',
+    price: '3.19 ETH',
+    likes: 27,
+  },
+  {
+    img: 'img/cover/cover2.jpeg',
+    time: 'an hour left',
+    title: 'Les Immortels, the Treachery of Artificial Shadows',
+    autor: '@l1rose',
+    price: '3.19 ETH',
+    likes: 27,
+  },
+];
